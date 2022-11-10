cc854b114e7a2d7c59a7de782e544d02324a9f77
Carlos Hidalgo <cfhidalgo10@gmail.com>
2022-11-10 10:02:46 -0600

    user associations written

--- models/user.js
+++ models/user.js
@@ -8,7 +8,12 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      User.hasMany(models.Event, { as: 'hosting', foreignKey: 'hostId' })
+      User.hasMany(models.Event, { as: 'attending', foreignKey: ['userId'] })
+      User.belongsToMany(models.Event, {
+        as: 'guests',
+        foreignKey: ['attending']
+      })
     }
   }
   User.init(
